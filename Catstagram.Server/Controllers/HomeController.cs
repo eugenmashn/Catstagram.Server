c414fbf36c34d3dd7e131c54fb2ab219fcf083f7
mashniskuy <mashniskuy@gmail.com>
2020-09-11 11:53:49 +0300

    Add project files.

---
+++ Catstagram.Server/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+namespace Catstagram.Server.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    public class HomeController : Controller
+    {
+        
+    }
+}
